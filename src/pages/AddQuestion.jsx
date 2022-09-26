3f7dc8db83d8f86aa460ad9d49284f63f82890c2
Cyfroni <karolplg@gmail.com>
2022-09-26 16:43:39 +0200

    simple styling

--- src/pages/AddQuestion.jsx
+++ src/pages/AddQuestion.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { getDatabase, ref, set, push } from "firebase/database";
+import "./AddQuestion.css";
 
 export default function AddQuestion() {
   const [question, setQuestion] = useState("");
@@ -18,22 +19,34 @@ export default function AddQuestion() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-      <input
-        type="text"
-        value={answers}
-        onChange={(e) => setAnswers(e.target.value)}
-      />
-      <input
-        type="text"
-        value={correct}
-        onChange={(e) => setCorrect(e.target.value)}
-      />
+    <div class="add-question">
+      <div>
+        <label htmlFor="question">Question</label>
+        <input
+          id="question"
+          type="text"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="answers">answers</label>
+        <input
+          id="answers"
+          type="text"
+          value={answers}
+          onChange={(e) => setAnswers(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="correct">correct</label>
+        <input
+          id="correct"
+          type="text"
+          value={correct}
+          onChange={(e) => setCorrect(e.target.value)}
+        />
+      </div>
       <button onClick={save}>save</button>
     </div>
   );
